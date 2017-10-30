5cfa48debb7ebdf5654ecb0bea920da63b5154e6
AntoinePlumerault <antoine.plumerault@gmail.com>
2017-10-30 19:53:20 +0100

    Adding Value Iteration

--- main_tp1_ex1.py
+++ main_tp1_ex1.py
@@ -1,6 +1,7 @@
 from mdp import MDP
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 P_x_a = np.array([[[0.45, 0.55, 0.00],
 		           [0.60, 0.40, 0.00],
@@ -18,26 +19,76 @@ X = range(env.n_states)
 A = range(env.n_actions)
 gamma = 0.95
 
-pi_opt = [1,1,0] 
+# ========= Policy evaluation ========= #
+
+pi_opt = [1,1,0]  # Optimal policy
+
 V_opt = np.array([0.0] * env.n_states)
-T = 100000
+T = 10000
 for t in range(T):
 	for x in X:
 		next_state, reward = env.step(x, pi_opt[x])
 		V_opt[x] = reward + gamma*sum(P_x_a[pi_opt[x], x, :] * V_opt[:]) # WARNING 
-Delta_V = []
+
+
+# ========== Value iteration ========== #
+t = time.time()
+
+Delta_V_Value = []
+
+V = np.array([0.0] * env.n_states)
+term = False
+while not term:
+	TV = np.array([0.0] * env.n_states)
+	for x in X:
+		TV[x] = -float('inf')
+		for a in A:
+			next_state, reward = env.step(x, a)
+			TVa = reward + gamma*sum(P_x_a[a, x, :] * V[:])
+			if TVa > TV[x]:
+				TV[x] = TVa
+
+	#print(max(abs(TV - V)))
+	if max(abs(TV - V)) < 0.01:
+		term = True
+
+	Delta_V_Value.append(max(abs(V_opt - V)))
+
+	V = TV
+
+pi = [A[0]] * env.n_states
+for x in X:
+	best_a = A[0]
+	best_Va = -float('inf')
+	for a in A:
+		next_state, reward = env.step(x, a)
+		Va = reward + gamma*sum(P_x_a[a, x, :] * V[:])
+		if Va > best_Va:
+			best_a = a
+			best_Va = Va
+	pi[x] = best_a
+
+elapsed = time.time() - t
+
+plt.plot(Delta_V_Value)
+
+print('elapsed Value Iteration', elapsed)
+
+# ========= Policy iteration ========== #
+
+t = time.time()
+
+Delta_V_Policy = []
 
 pi = [A[0]] * env.n_states
 V = np.array([0.0] * env.n_states)
 term = False
 while (not term):
-	#print('loop')
-	new_V = np.array([0.0] * env.n_states)
+	V_pi = np.array([0.0] * env.n_states)
 	for x in X:
 		next_state, reward = env.step(x, pi[x])
-		new_V[x] = reward + gamma*sum(P_x_a[pi[x], x, :] * V[:])
+		V_pi[x] = reward + gamma*sum(P_x_a[pi[x], x, :] * V[:])
 
-	#print(new_V)
 	for x in X:
 		best_a = A[0]
 		best_Va = -float('inf')
@@ -49,17 +100,28 @@ while (not term):
 				best_Va = Va
 		pi[x] = best_a
 	
-	Delta_V.append(max(abs(V_opt - V)))
+	Delta_V_Policy.append(max(abs(V_opt - V)))
 	
-	if max(abs(new_V - V)) < 0.01:
+	if max(abs(V_pi - V)) < 0.01:
 		term = True
 
-	V = new_V
+	V = V_pi
+
+elapsed = time.time() - t
+
+plt.plot(Delta_V_Policy)
+
+print(len(Delta_V_Policy),len(Delta_V_Value))
+print('elapsed Policy Iteration', elapsed)
+
+# ====================================== #
+
+plt.show()
+
 print(2*0.01 * gamma / (1.0-gamma))
 print(pi)
 print(V)
 print(V_opt)
-#print(Delta_V)
-plt.plot(Delta_V)
-plt.show()
+
+
 
